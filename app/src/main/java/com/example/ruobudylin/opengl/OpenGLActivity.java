3e43a69704ffc9c15221f86eb68deaf998e2302f
ruobudylin <Oleg.Budylin@harman.com>
2016-10-04 16:42:48 +0300

    init

---
+++ app/src/main/java/com/example/ruobudylin/opengl/OpenGLActivity.java
@@ -0,0 +1,38 @@
+package com.example.ruobudylin.opengl;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class OpenGLActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        if (supportsEs2) {
+                // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+                // Assign our renderer.
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        setContentView(glSurfaceView);
+    }
+}
